c1f83f2687b932e7b65d0730fcb8e5b1fd4bbb27
Mulalo mphidi <mphidi@gmail.com>
2016-03-23 21:14:36 +0200

    Fields defined

--- smscode/smscode/smscode/FieldsDef.xaml.cs
+++ smscode/smscode/smscode/FieldsDef.xaml.cs
@@ -20,127 +20,107 @@ namespace smscode
             var table = new TableView();
             table.Intent = TableIntent.Settings;
 
-            var field01 = new StackLayout() { Orientation = StackOrientation.Horizontal };
-            var field02 = new StackLayout() { Orientation = StackOrientation.Horizontal };
-            var field03 = new StackLayout() { Orientation = StackOrientation.Horizontal };
-            var field04 = new StackLayout() { Orientation = StackOrientation.Horizontal };
-            var field05 = new StackLayout() { Orientation = StackOrientation.Horizontal };
-            var field06 = new StackLayout() { Orientation = StackOrientation.Horizontal };
-            var field07 = new StackLayout() { Orientation = StackOrientation.Horizontal };
-            var field08 = new StackLayout() { Orientation = StackOrientation.Horizontal };
-            var field09 = new StackLayout() { Orientation = StackOrientation.Horizontal };
+            var fields = new StackLayout() { Orientation = StackOrientation.Vertical };
+
+            fields.Children.Add(new Label()
+            {
+                Text = "Fields",
+                TextColor = Color.FromHex("#f35e20"),
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            });
 
             if (numberOfFields-- > 0)
             {
-                field01.Children.Add(new Entry()
+                fields.Children.Add(new Entry()
                 {
                     Text = "Field1",
                     TextColor = Color.FromHex("#f35e20"),
-                    HorizontalOptions = LayoutOptions.EndAndExpand
-                });    //VerticalOptions = LayoutOptions.Center
+                    HorizontalOptions = LayoutOptions.FillAndExpand
+                });    
             }
 
             if (numberOfFields-- > 0)
             {
-                field02.Children.Add(new Entry()
+                fields.Children.Add(new Entry()
                 {
                     Text = "Field2",
                     TextColor = Color.FromHex("#f35e20"),
-                    HorizontalOptions = LayoutOptions.EndAndExpand
-                    //VerticalOptions = LayoutOptions.Center
+                    HorizontalOptions = LayoutOptions.FillAndExpand
                 });
             }
 
             if (numberOfFields-- > 0)
             {
 
-                field03.Children.Add(new Entry()
+                fields.Children.Add(new Entry()
                 {
                     Text = "Field3",
                     TextColor = Color.FromHex("#f35e20"),
-                    HorizontalOptions = LayoutOptions.EndAndExpand
+                    HorizontalOptions = LayoutOptions.FillAndExpand
                 });
             }
 
             if (numberOfFields-- > 0)
             {
-                field04.Children.Add(new Entry()
+                fields.Children.Add(new Entry()
                 {
                     Text = "Field4",
                     TextColor = Color.FromHex("#f35e20"),
-                    HorizontalOptions = LayoutOptions.EndAndExpand
+                    HorizontalOptions = LayoutOptions.FillAndExpand
                 });
             }
 
             if (numberOfFields-- > 0)
             {
-                field05.Children.Add(new Entry()
+                fields.Children.Add(new Entry()
                 {
                     Text = "Field5",
                     TextColor = Color.FromHex("#f35e20"),
-                    HorizontalOptions = LayoutOptions.EndAndExpand
+                    HorizontalOptions = LayoutOptions.FillAndExpand
                 });
             }
 
             if (numberOfFields-- > 0)
             {
-                field06.Children.Add(new Entry()
+                fields.Children.Add(new Entry()
                 {
                     Text = "Field6",
                     TextColor = Color.FromHex("#f35e20"),
-                    HorizontalOptions = LayoutOptions.EndAndExpand
+                    HorizontalOptions = LayoutOptions.FillAndExpand
                 });
             }
 
             if (numberOfFields-- > 0)
             {
-                field07.Children.Add(new Entry()
+                fields.Children.Add(new Entry()
                 {
                     Text = "Field7",
                     TextColor = Color.FromHex("#f35e20"),
-                    HorizontalOptions = LayoutOptions.EndAndExpand
+                    HorizontalOptions = LayoutOptions.FillAndExpand
                 });
             }
 
             if (numberOfFields-- > 0)
             {
-                field08.Children.Add(new Entry()
+                fields.Children.Add(new Entry()
                 {
                     Text = "Field8",
                     TextColor = Color.FromHex("#f35e20"),
-                    HorizontalOptions = LayoutOptions.EndAndExpand
+                    HorizontalOptions = LayoutOptions.FillAndExpand
                 });
             }
 
             if (numberOfFields-- > 0)
             {
-                field09.Children.Add(new Entry()
+                fields.Children.Add(new Entry()
                 {
                     Text = "Field9",
                     TextColor = Color.FromHex("#f35e20"),
-                    HorizontalOptions = LayoutOptions.EndAndExpand
+                    HorizontalOptions = LayoutOptions.FillAndExpand
                 });
             }
 
-            table.Root = new TableRoot()
-                    {
-                        new TableSection("Fields")
-                        {
-                            new ViewCell() {View = field01},
-                            new ViewCell() {View = field02},
-                            new ViewCell() {View = field03},
-                            new ViewCell() {View = field04},
-                            new ViewCell() {View = field05},
-                            new ViewCell() {View = field06},
-                            new ViewCell() {View = field07},
-                            new ViewCell() {View = field08},
-                            new ViewCell() {View = field09}
-
-
-                        }
-
-                      };
-            Content = table;
+            Content = fields;
         }
     }
 }

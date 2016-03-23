f1707d5cf82b6eb13ecc6fcc9748bc66e55a639b
Mulalo mphidi <mphidi@gmail.com>
2016-03-23 20:08:02 +0200

    First Update

---
+++ smscode/smscode/smscode/homepage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace smscode
+{
+    public partial class homepage : ContentPage
+    {
+        public homepage()
+        {
+            InitializeComponent();
+        }
+
+        private async void navProjectFieldsDef(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FieldsDef(entryProjectName.Text, pickerNumberOfFields.Items[pickerNumberOfFields.SelectedIndex], pickerSeparator.Items[pickerSeparator.SelectedIndex]));
+            //await Navigation.PushAsync(new FieldsDef("tested well"));
+
+        }
+    }
+
+}
